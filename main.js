1e88f559d5fcea810d4d4960eb0e2f70da90b042
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-11-20 18:53:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+song1="";
+song2="";
 function setup() {
     canvas=createCanvas(550,550);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video,0,0,550,550)
+}
+function preload() {
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+function play() {
+    song1.play();
 }
