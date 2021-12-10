187645e6f88484f82a3a4a49dd1756e16d87c5a5
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-10 20:03:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+song1_status="";
+score_leftWrist=0;
 function setup() {
     canvas=createCanvas(550,550);
     canvas.center();
@@ -17,6 +19,17 @@ function modelLoaded() {
 }
 function draw() {
     image(video,0,0,550,550)
+    song1_status=song1.isPlaying();
+    fill("#0501FF");
+    stroke("#0501FF");
+    if(score_leftWrist > 0.2) {
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        if (song1_status ==false) {
+            song1.play();
+            document.getElementById("song").innerHTML="Playing Peter Pan";
+        }
+    }
 }
 function preload() {
     song1=loadSound("music.mp3");
@@ -30,6 +43,7 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        score_leftWrist=results[0].pose.keypoints[9].score;
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX+",leftWristY ="+ leftWristY);
